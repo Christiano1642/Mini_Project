ddb0b55e08bca3298721d79fa6fcea03af7ea4b2
Christian Wallace <christian.wallace@student.swic.edu>
2021-11-17 13:55:09 -0600

    added post, delete, put for the listing and review with comments

--- server/app/router.js
+++ server/app/router.js
@@ -10,6 +10,7 @@ router.get("/", (_, res) =>{
     res.send("Hello from API");
 });
 
+// route for finding words that match name and description.
 router.get("/listings", async (req, res) =>{
     const currentListings = await client
     .db(config.db.name)
@@ -18,12 +19,14 @@ router.get("/listings", async (req, res) =>{
         {name: {$regex: (req.body.keywords? req.body.keywords: ""), $options: "i"}},
         {description:{ $regex:(req.body.keywords? req.body.keywords: ""), $options: "i"}},
     ]})
+    .max({$max:{price: req.query.price? req.query.price: 0}})
     .limit(req.query.limit? req.query.limit: 0)
     .toArray();
 
     res.json(currentListings);
 })
 
+// Route to return all details of an id
 router.get("/:id", async (req, res) => {
    const listingId = await client
    .db(config.db.name)
@@ -34,6 +37,7 @@ router.get("/:id", async (req, res) => {
    res.json(listingId);
 })
 
+// Route to return only the review of a given id
 router.get("/review/:id", async (req, res) => {
     const review = await client
     .db(config.db.name)
@@ -43,7 +47,8 @@ router.get("/review/:id", async (req, res) => {
     res.json(review.review);
 })
 
-router.post("/listing/:id", async (req, res) => {
+// Route to post a review
+router.post("/review/:id", async (req, res) => {
     const listing = await client
     .db(config.db.name)
     .collection(config.db.collection)
@@ -54,4 +59,55 @@ router.post("/listing/:id", async (req, res) => {
         res.json(listing);
 })
 
+// Route to insert a new listing
+router.post("/review/:id/", async (req, res) => {
+    const insertListing = await client
+    .db(config.db.name)
+    .collection(config.db.collection)
+    .insertOne(req.body)
+
+    res.json(insertListing);
+})
+
+// Delete a listing
+router.delete("/listing/:id", async (req, res) => {
+    const deleteListing = await client
+    .db(config.db.name)
+    .collection(config.db.collection)
+    .deleteOne({_id: req.params.id})
+
+    res.json(deleteListing);
+})
+
+// Update a listing
+router.put("/listing/", async (req, res) => {
+    const updateListing = await client
+    .db(config.db.name)
+    .collection(config.db.collection)
+    .updateOne({_id: req.params.id})
+
+    res.json(updateListing);
+})
+
+// Delete a review
+router.delete("/review/:id", async (req, res) => {
+    const deleteReview = await client
+    .db(config.db.name)
+    .collection(config.db.collection)
+    .deleteOne({_id: req.params.id},
+        {$pull:{rewiews: req.body}})
+    
+        res.json(deleteReview);
+})
+
+// Update a review
+router.put("/review/", async (req, res) =>{
+    const updateReview = await client
+    .db(config.db.name)
+    .collection(config.db.collection)
+    .updateOne({_id: req.params.id},
+        {$set:{rewiews: req.body}})
+
+        res.json(updateReview);
+})
 export default router;
