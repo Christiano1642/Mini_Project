7f1cae29a4677568c870378861d5b910034fed19
Christian Wallace <christian.wallace@student.swic.edu>
2021-11-17 09:58:02 -0600

    destructured db with name and collection

--- server/app/config.js
+++ server/app/config.js
@@ -1,13 +1,12 @@
-// Provides config for the app in conjunction with dotenv
-// Config is the only place accesses 'dotenv'
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export default {
-  port: process.env.PORT || 3000,
-  db:
-    process.env.DB_CLIENT_URL ||
-    // Use localhost if no DB_CLIENT_URL is provided
-    "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000",
+port: process.env.PORT || 3000,
+db: {
+    clientURL: process.env.DB_CLIENT_URL,
+    name: "sample_airbnb",
+    collection: "listingsAndReviews"
+},
 };
